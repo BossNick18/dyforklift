ea36911262723127b4d527b5bbb4b5fa4acb644a
BossNick18 <nick@nickconsultinggroup.com>
2024-05-19 01:39:52 +0800

    Updates

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,28 @@
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBKrtlWdUW7zO9g9zm9slaguHQmF6kJuV0",
+  authDomain: "diyouforklfitapp.firebaseapp.com",
+  projectId: "diyouforklfitapp",
+  storageBucket: "diyouforklfitapp.appspot.com",
+  messagingSenderId: "728063402975",
+  appId: "1:728063402975:web:ef09e7cb6df5ef0ddcda6a",
+  measurementId: "G-2QJDPH5EGW"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/firebase-logo.png'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
